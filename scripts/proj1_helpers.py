76a001ae830d49228df86e7964695cfc6b6ee518
GonxheIdrizi <gonxhe.idrizi@epfl.ch>
2019-10-20 13:55:03 +0200

    temp

--- scripts/proj1_helpers.py
+++ scripts/proj1_helpers.py
@@ -5,10 +5,10 @@ import numpy as np
             
 def load_csv_data(data_path, sub_sample=False):
     """Loads data and returns y (class labels), tX (features) and ids (event ids)"""
-    y = np.genfromtxt(data_path, delimiter=",", skip_header=1, dtype=str, usecols=1)
-    x = np.genfromtxt(data_path, delimiter=",", skip_header=1)
+    y = np.genfromtxt(data_path, delimiter=",", skip_header=0, dtype=str, usecols=1)
+    x = np.genfromtxt(data_path, delimiter=",", skip_header=0)
     ids = x[:, 0].astype(np.int)
-    input_data = x[:, 2:]
+    input_data = x[:, 1:]
 
     # convert class labels from strings to binary (-1,1)
     yb = np.ones(len(y))
